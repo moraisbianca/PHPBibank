6921efc799be6a8c4317755ec7bc4ff80086a8f4
moraisbianca <biancacamilamorais@gmail.com>
2023-09-25 14:11:22 -0300

    First Commit

---
+++ routes.php
@@ -0,0 +1,15 @@
+<?php
+//use App\Controller\{};
+
+$parse_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch($parse_uri) {
+
+    case "/":
+        //Controller::form();
+    break;
+    
+    default:
+        header("Location: /");
+break;
+}
